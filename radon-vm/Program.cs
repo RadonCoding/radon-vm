52c354e8fe20ab4e97d8a4160b046f29e60652f0
RadonCoding <henry.welch@reborn.com>
2023-08-05 13:47:41 +0300

    Fix code

--- radon-vm/Program.cs
+++ radon-vm/Program.cs
@@ -1,26 +1,32 @@
 ﻿namespace radon_vm {
     internal class Program {
-        private static void Main(string[] args) {
+        private static void Main(string[] args)
+        {
             string inputPath = args[0];
             string? inputDir = Path.GetDirectoryName(inputPath);
 
-            if (string.IsNullOrEmpty(inputDir)) {
+            if (string.IsNullOrEmpty(inputDir))
+            {
                 return;
             }
-            
+
             string outputDir = Path.Combine(inputDir, "Protected");
             string outputPath = Path.Combine(outputDir, Path.GetFileName(inputPath));
 
-
-            try {
+            try
+            {
                 Directory.CreateDirectory(outputDir);
-    
+
                 File.Copy(inputPath, outputPath, true);
-    
+
                 Compiler compiler = new Compiler(outputPath);
                 compiler.Protect();
                 compiler.Save();
-        } catch (Exception e) {
-                Console.WriteLine($"File error: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
